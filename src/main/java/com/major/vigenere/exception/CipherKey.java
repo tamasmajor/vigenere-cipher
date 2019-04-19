9f7ca4f9c08a713f16d0eff053a8c572a12ad629
tamasmajor <majortamas8@gmail.com>
2019-04-19 19:02:00 +0200

    Implemented the CipherKey which help with the shifting during the encryption and decryption

--- src/main/java/com/major/vigenere/exception/CipherKey.java
+++ src/main/java/com/major/vigenere/exception/CipherKey.java
@@ -2,12 +2,36 @@ package com.major.vigenere.exception;
 
 public class CipherKey {
     private final String key;
+    private final int[] shifting;
 
-    public CipherKey(String key) {
+    public CipherKey(String key, char[] alphabet) {
         this.key = key;
+        this.shifting = calculateShifting(key, alphabet);
     }
 
     public String getKey() {
         return key;
+    }
+
+    public int shiftingAtPosition(int overallPosition) {
+        int keyPosition = overallPosition % key.length();
+        return shifting[keyPosition];
+    }
+
+    private int[] calculateShifting(String key, char[] alphabet) {
+        int[] shifting = new int[key.length()];
+        for (int i = 0; i < shifting.length; i++) {
+            shifting[i] = alphabetIndex(key.charAt(i), alphabet);
+        }
+        return shifting;
+    }
+
+    private int alphabetIndex(char c, char[] alphabet) {
+        for (int i = 0; i < alphabet.length; i++) {
+            if (c == alphabet[i]) {
+                return i;
+            }
+        }
+        throw new IllegalArgumentException("Cipher key must be constructed using chars from the alphabet");
     }
 }
--- src/test/java/com/major/vigenere/exception/CipherKeyTest.java
+++ src/test/java/com/major/vigenere/exception/CipherKeyTest.java
@@ -1,15 +1,70 @@
 package com.major.vigenere.exception;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DisplayName("CipherKey")
 public class CipherKeyTest {
 
-    @Test
-    public void getKey_constructedWithKey_returnsKey() {
-        CipherKey cipherKey = new CipherKey("CIPHERKEY", new char[0]);
-        assertEquals("CIPHERKEY", cipherKey.getKey());
+    @Nested
+    @DisplayName("Key")
+    class GetKeyTests {
+
+        @Test
+        public void constructedWithKey_returnsKey() {
+            CipherKey cipherKey = new CipherKey("KEY", new char[] { 'E', 'K', 'Y' });
+            assertEquals("KEY", cipherKey.getKey());
+        }
+
+        @Test
+        public void keyContainsLetterOutsideOfAlphabet_throwsException() {
+            Assertions.assertThrows(IllegalArgumentException.class, () -> new CipherKey("KEY", new char[] { 'K', 'E' }));
+        }
+    }
+
+    @Nested
+    @DisplayName("Shifting calculation")
+    class ShiftingCalculationTests {
+
+        @Test
+        public void alphabetHasOneLetter_shiftingIsZero() {
+            CipherKey cipherKey = new CipherKey("A", new char[] { 'A' });
+            assertEquals(0, cipherKey.shiftingAtPosition(0));
+        }
+
+        @Nested
+        @DisplayName("Alphabet with multiple letters")
+        class MultiLetterAlphabet {
+
+            final String key = "EBD";
+            final char[] alphabet = new char[] { 'A', 'B', 'C', 'D', 'E' };
+            final CipherKey cipherKey = new CipherKey(key, alphabet);
+
+            @Test
+            public void overallPositionNotBiggerThanKeyLength_shiftingIsCurrentKeyLettersPositionInAlphabet() {
+                assertEquals(4, cipherKey.shiftingAtPosition(0)); // (E)BD
+                assertEquals(1, cipherKey.shiftingAtPosition(1)); // E(B)D
+                assertEquals(3, cipherKey.shiftingAtPosition(2)); // EB(D)
+            }
+            
+            @Test
+            public void overallPositionIsMultipleOfKeyLength_shiftingIsAlwaysPositionOfFirstKeyCharInAlphabet() {
+                assertEquals(4, cipherKey.shiftingAtPosition(0)); // (E)BD
+                assertEquals(4, cipherKey.shiftingAtPosition(3)); // EBD(E)
+                assertEquals(4, cipherKey.shiftingAtPosition(6)); // EBDEBD(E)
+            }
+
+            @Test
+            public void overallPositionIsBiggerThenKeyLength_usedPositionIsUsingKeyLengthModulo() {
+                assertEquals(1, cipherKey.shiftingAtPosition(4));
+                assertEquals(3, cipherKey.shiftingAtPosition(11));
+            }
+        }
+
     }
 
 }
